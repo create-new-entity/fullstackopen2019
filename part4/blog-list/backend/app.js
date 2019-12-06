358bb1129b5c553f6ea70fbef2f7a71dd1550873
Pavel <imran.pavel@gmail.com>
2019-12-06 18:20:27 +0200

    4.1 - 4.2

---
+++ part4/blog-list/backend/app.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const router = require('./controllers/routes');
+const config = require('./utils/config');
+const errorMiddleware = require('./utils/errorMiddleware');
+
+mongoose
+  .connect(config.MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('DB Connected');
+  })
+  .catch((error) => {
+    console.log('DB Connection failed');
+  });
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api/blogs', router);
+app.use(errorMiddleware.unknownEndpoint);
+app.use(errorMiddleware.errorHandler);
+
+module.exports = app;
---
+++ part4/blog-list/backend/index.js
@@ -0,0 +1,13 @@
+
+const http = require('http');
+const app = require('./app');
+const config = require('./utils/config');
+
+const server = http.createServer(app);
+
+server.listen(config.PORT, () => {
+  console.log(`Server listening on port ${config.PORT}`);
+});
+
+
+module.exports = app;
