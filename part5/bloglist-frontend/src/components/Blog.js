60dfb02f0a8a7a6f09506f2a0a41907b99377665
Pavel <imran.pavel@gmail.com>
2019-12-22 14:53:52 +0200

    5.6

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  let showOrHide = { display: 'none'};
+  const [visible, setVisible] = useState(false);
+  const toggle = () => {
+    setVisible(!visible);
+  };
+
+  let author = null;
+  if(blog.user){
+    author = <p>Added by {blog.user.name}</p> 
+  }
+  
+  visible ? showOrHide = { display: ''} : showOrHide = {display: 'none'};
+  return (
+    <div className="blog" onClick={toggle}>
+      <div>{blog.title}</div>
+      <div style={showOrHide}>
+          <a href={blog.url}>{blog.url}</a>
+          <p>{blog.likes} like(s)</p><button>Like</button>
+          <p>Creator: {blog.author}</p>
+          {author}
+      </div>
+    </div>
+  );
+};
 
 export default Blog
