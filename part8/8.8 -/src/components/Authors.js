d13c353cbe4dc14b479993d011fdbaaf00b4b6d0
Pavel <imran.pavel@gmail.com>
2020-01-08 12:01:08 +0200

    8.8

---
+++ part8/8.8 -/src/components/Authors.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { GET_ALL_AUTHORS } from './../queries';
+import { useLazyQuery } from '@apollo/client';
+
+const Authors = (props) => {
+  const [ getAllAuthors, result ] = useLazyQuery(GET_ALL_AUTHORS);
+  
+  if (!props.show) {
+    return null;
+  }
+  
+  if(!result.called) {
+    getAllAuthors();
+    return <div>Loading...</div>;
+  };
+  
+  if(result.loading) return <div>Still loading...</div>;
+  let authors = result.data.allAuthors;
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
